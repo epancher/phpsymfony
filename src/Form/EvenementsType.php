b2977f86eda60b3d57e7b5dcd5b1975d9cc06eb0
edgar <edgar.pancher@maformationenligne.bzh>
2019-05-23 16:38:46 +0200

    correction bugs

--- src/Form/EvenementsType.php
+++ src/Form/EvenementsType.php
@@ -17,6 +17,8 @@ class EvenementsType extends AbstractType
             ->add('txt_evnmt')
             ->add('date_evnmt')
             ->add('heure_evnmt')
+            ->add('lat')
+            ->add('lng')
             ->add('pictureFiles', FileType::class, [
                 'required' => false,
                 'multiple' => true
---
+++ src/Migrations/Version20190523140546.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190523140546 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE evenements ADD lat DOUBLE PRECISION NOT NULL, ADD lng DOUBLE PRECISION NOT NULL, CHANGE updated_at updated_at DATETIME');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE evenements DROP lat, DROP lng, CHANGE updated_at updated_at DATETIME DEFAULT NULL');
+    }
+}
